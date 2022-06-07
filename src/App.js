15647b5d536926ebc60a662c16a69201d21bb50e
chan <chan157@naver.com>
2022-06-07 19:45:13 +0900

    Add Modal Component

--- src/App.js
+++ src/App.js
@@ -42,9 +42,21 @@ function App() {
         <h4>{ title[2] }</h4>
         <p>2월 17일 발행</p>
       </div>
-      <h4>{ post }</h4>
+      
+      <Modal></Modal>
+
     </div>
   );
+}
+
+function Modal(){
+  return (
+    <div className="modal">
+        <h4>제목</h4>
+        <p>날짜</p>
+        <p>상세내용</p>
+    </div>
+  )
 }
 
 export default App;
